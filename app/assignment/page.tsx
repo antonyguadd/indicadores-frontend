211266e2ffe3fe3d25c17f50b896c51e2a2d98e2
Antonio Guadalupe Flores Torres <antonyguad@gmail.com>
2024-10-21 12:53:37 -0600

    estilos1

--- app/assignment/page.tsx
+++ app/assignment/page.tsx
@@ -1,9 +1,9 @@
 "use client"
 
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import Link from "next/link";
 import { usePathname } from "next/navigation"; // Importa usePathname
-import { LayoutDashboard, FileText, Users, Settings, Menu, Bell, User, ChevronLeft, ChevronRight, Upload, FilePlus, Sun, Moon } from "lucide-react";
+import { LayoutDashboard, FileText, Settings, Menu, Bell, User, ChevronLeft, ChevronRight, Upload, FilePlus, Sun, Moon } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
